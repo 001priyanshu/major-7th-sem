d9433e72b2062487db28b25afff698f78bff6dd0
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2023-12-04 22:45:04 +0530

    done

--- logistic.py
+++ logistic.py
@@ -1,44 +1,36 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.keras.models import Sequential
-from sklearn.keras.layers import LSTM
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
+# Assuming 'output_combined.xlsx' contains your dataset
 dummy_data = pd.read_excel('output_combined.xlsx')
 
 X = dummy_data.drop(['SubjectID', 'Label'], axis=1)
 y = dummy_data['Label']
 
+# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Standardize the features
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Reshape the data to fit the LSTM model
-X_train_scaled = X_train_scaled.reshape((X_train_scaled.shape[0], 1, X_train_scaled.shape[1]))
-X_test_scaled = X_test_scaled.reshape((X_test_scaled.shape[0], 1, X_test_scaled.shape[1]))
+# Use Logistic Regression
+model = LogisticRegression(random_state=42)
+model.fit(X_train_scaled, y_train)
 
-# Build the RNN model
-model = Sequential()
-model.add(LSTM(64, return_sequences=True, input_shape=(X_train_scaled.shape[1], 1)))
-model.add(LSTM(32))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# Train the model
-model.fit(X_train_scaled, y_train, epochs=10, batch_size=32, validation_split=0.2)
-
-# Make predictions
+# Make predictions on the test set
 y_pred = model.predict(X_test_scaled)
-y_pred = (y_pred > 0.5).astype(int)
 
 # Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
 conf_matrix = confusion_matrix(y_test, y_pred)
 classification_rep = classification_report(y_test, y_pred)
 
+# Print the results
 print(f'Accuracy: {accuracy:.2f}')
 print('\nConfusion Matrix:')
 print(conf_matrix)
--- support-vector.py
+++ support-vector.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.keras.models import Sequential
-from sklearn.keras.layers import LSTM
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 dummy_data = pd.read_excel('output_combined.xlsx')
@@ -16,25 +15,11 @@ scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Reshape the data to fit the LSTM model
-X_train_scaled = X_train_scaled.reshape((X_train_scaled.shape[0], 1, X_train_scaled.shape[1]))
-X_test_scaled = X_test_scaled.reshape((X_test_scaled.shape[0], 1, X_test_scaled.shape[1]))
+model = SVC(random_state=42)
+model.fit(X_train_scaled, y_train)
 
-# Build the RNN model
-model = Sequential()
-model.add(LSTM(64, return_sequences=True, input_shape=(X_train_scaled.shape[1], 1)))
-model.add(LSTM(32))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# Train the model
-model.fit(X_train_scaled, y_train, epochs=10, batch_size=32, validation_split=0.2)
-
-# Make predictions
 y_pred = model.predict(X_test_scaled)
-y_pred = (y_pred > 0.5).astype(int)
 
-# Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
 conf_matrix = confusion_matrix(y_test, y_pred)
 classification_rep = classification_report(y_test, y_pred)
